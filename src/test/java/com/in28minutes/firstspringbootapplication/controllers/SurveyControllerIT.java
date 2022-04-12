aa9eb6c68b8694afc23320bfde4d71c4c03d46a1
nikhil-teja <nikhil.teja@paymatrix.in>
2022-04-12 16:52:30 +0530

    test for add question method

--- src/test/java/com/in28minutes/firstspringbootapplication/controllers/SurveyControllerIT.java
+++ src/test/java/com/in28minutes/firstspringbootapplication/controllers/SurveyControllerIT.java
@@ -7,17 +7,21 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
+import java.util.List;
 
 import com.in28minutes.firstspringbootapplication.*;
+import com.in28minutes.firstspringbootapplication.model.Question;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = FirstspringbootapplicationApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,7 +38,6 @@ public class SurveyControllerIT {
         TestRestTemplate restTemplate = new TestRestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
-
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
         HttpEntity<String> entity = new HttpEntity<String>(null, headers);
@@ -44,5 +47,29 @@ public class SurveyControllerIT {
         String expected = "{id:Question1}";
 
         JSONAssert.assertEquals(expected, response.getBody(), false);
+    }
+
+    @Test
+    public void testAddQuestion() throws JSONException {
+
+        String url = "http://localhost:" + port + "/surveys/Survey1/questions";
+
+        TestRestTemplate restTemplate = new TestRestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        Question question = new Question("doesn't matter", "dummy description", "correctAnswer",
+                Arrays.asList("option1", "option2", "correctAnswer", "option4"));
+
+        HttpEntity<Question> entity = new HttpEntity<Question>(question, headers);
+
+        ResponseEntity<List<Question>> response = restTemplate.exchange(url,
+                HttpMethod.POST, entity, new ParameterizedTypeReference<List<Question>>() {
+                });
+
+        String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+
+        assertTrue(actual.contains("surveys/Survey1/questions"));
     }
 }
