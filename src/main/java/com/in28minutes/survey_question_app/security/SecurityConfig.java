32261b0266e95c24ad1f7dfe6b8d324f7db593be
nikhil teja <nikhiltejagudla@gmail.com>
2022-05-16 22:11:21 +0530

    user athentication added

---
+++ src/main/java/com/in28minutes/survey_question_app/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.in28minutes.survey_question_app.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance())
+        .withUser("user1").password("secret1").roles("USER")
+        .and().withUser("admin1").password("secret1").roles("USER", "ADMIN");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception{
+
+        http.httpBasic().and().authorizeRequests()
+        .antMatchers("/surveys/**").hasRole("USER")
+        .antMatchers("/users/**").hasRole("USER")
+        .antMatchers("/**").hasRole("ADMIN")
+        .and().csrf().disable()
+        .headers().frameOptions().disable();
+    }
+}
